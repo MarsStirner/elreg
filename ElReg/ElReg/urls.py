e90d74ff58e442fb86c89bec9d80442d54d8cbc6
Vintorez <Okulichev@rambler.ru>
2012-09-20 18:08:55 +0400

    Добавлены новые url-адреса

--- ElReg/ElReg/urls.py
+++ ElReg/ElReg/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from elreg_app.views import view_index, view_lpu
+from elreg_app.views import view_index, view_lpu, view_podrazdelenie
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', view_index.index, name='index'),
-    url(r'^lpu/(\d{10,11})/$', view_lpu.index, name='lpu')
+    url(r'^lpu/$', view_lpu.index, name='lpu'),
+    url(r'^lpu/(\d{10,11})/$', view_lpu.index),
+    url(r'^podrazdelenie/(\d{1,2})/$', view_podrazdelenie.index),
     # Examples:
     # url(r'^$', 'ElReg.views.home', name='home'),
     # url(r'^ElReg/', include('ElReg.foo.urls')),
