e8fec26acf75464b14aa1829f4b7b6ea5be4e540
Vintorez <Okulichev@rambler.ru>
2012-09-22 12:48:33 +0400

    Добавлена модель для таблицы БД для соответствия региона и его кода

--- ElReg/elreg_app/models.py
+++ ElReg/elreg_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Region (models.Model):
+    region = models.CharField(max_length=100,)
+    code = models.PositiveIntegerField(default=0)
+    activation = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.region
+
+    class Meta:
+        ordering = ('-activation', 'region')
