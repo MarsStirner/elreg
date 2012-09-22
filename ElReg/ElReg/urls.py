722b6fbc81dfd8b3f25909fe5c80e4af347b9a2a
Vintorez <Okulichev@rambler.ru>
2012-09-22 22:04:41 +0400

    Добавлена возможность передавать имя шаблона

--- ElReg/ElReg/urls.py
+++ ElReg/ElReg/urls.py
@@ -1,14 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from elreg_app.views import view_index, view_lpu, view_podrazdelenie
+from elreg_app.views import view_lpu, view_podrazdelenie
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', view_index.index, name='index'),
-    url(r'^lpu/$', view_lpu.index, name='lpu'),
-    url(r'^lpu/(\d{10,11})/$', view_lpu.index),
-    url(r'^podrazdelenie/(\d{1,2})/$', view_podrazdelenie.index),
+    url(r'^$', view_lpu.index, {'template_name': 'index.html'}, name="index"),
+    url(r'^lpu/(?P<okato>0|\d{11})/$', view_lpu.index, {'template_name': 'lpu.html'}, name="lpu"),
+    url(r'^podrazdelenie/(?P<podrazd>\d{1,2})/$', view_podrazdelenie.index, {'template_name': 'podrazdelenie.html'}),
     # Examples:
     # url(r'^$', 'ElReg.views.home', name='home'),
     # url(r'^ElReg/', include('ElReg.foo.urls')),
