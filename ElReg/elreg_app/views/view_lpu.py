7011d4993da9c4b56181fca75e06741ca0bccdfa
Vintorez <Okulichev@rambler.ru>
2012-09-22 16:09:28 +0400

    Изменена функциональность

--- ElReg/elreg_app/views/view_lpu.py
+++ ElReg/elreg_app/views/view_lpu.py
@@ -3,20 +3,24 @@
 
 from django.shortcuts import render_to_response
 from suds.client import Client
-from lxml import etree
+from elreg_app.models import Region
 
-def index(request, okato='0000000000'):
-    base_url = "http://10.1.2.107/int-server/index.php?wsdl=list"
-    client = Client(base_url)
-    try:
-        hospitals_list = client.service.listHospitals(ocatoCode=okato).hospitals
-    except:
-        hospitals_list = ''
-    tree = etree.parse("elreg_app/regions.xml")
-    names, codes = [], []
-    for i in tree.xpath('/regions/region/name'):
-        names.append(i.text)
-    for i in tree.xpath('/regions/region/code'):
-        codes.append(i.text)
-    x = names[codes.index(okato)]
-    return render_to_response('lpu.html', {'hospitals_list': hospitals_list, 'x': x})
+
+def index(request, template_name, okato='0'):
+    hospitals_list = []
+    if okato == '0':
+        base_url = "http://10.1.2.107/int-server/index.php?wsdl=info"
+        client = Client(base_url)
+        info = client.service.getHospitalInfo()
+        for x in range(len(info)):
+            hospitals_list.append(info[x])
+    else:
+        base_url = "http://10.1.2.107/int-server/index.php?wsdl=list"
+        client = Client(base_url)
+        try:
+            hospitals_list = client.service.listHospitals(ocatoCode=okato).hospitals
+        except:
+            hospitals_list = ''
+    current_region = '' if okato == '0' else Region.objects.get(code=okato)
+    region_list = Region.objects.filter(activation=True)
+    return render_to_response(template_name, {'hospitals_list': hospitals_list, 'region_list': region_list, 'current_region': current_region})
--- ElReg/elreg_app/views/view_podrazdelenie.py
+++ ElReg/elreg_app/views/view_podrazdelenie.py
@@ -4,7 +4,7 @@
 from django.shortcuts import render_to_response
 from suds.client import Client
 
-def index(request, podrazd='00'):
+def index(request, template_name, podrazd='00'):
     base_url = "http://10.1.2.107/int-server/index.php?wsdl=info"
     client = Client(base_url)
     try:
@@ -18,5 +18,12 @@ def index(request, podrazd='00'):
             for b in c.buildings:
                 podrazdelenie_list.append(b.title)
 
-
-    return render_to_response('podrazdelenie.html', {'podrazdelenie_list': podrazdelenie_list, 'current_lpu': current_lpu})
+    new_list = []
+    base_url = "http://10.1.2.107/int-server/index.php?wsdl=list"
+    client = Client(base_url)
+    y = client.service.listHospitals().hospitals
+    for v in y:
+        for w in podrazdelenie_list:
+            if v.uid.startswith('%s'%(podrazd)) and v.title == w.title:
+                new_list.append(v.uid)
+    return render_to_response(template_name, {'podrazdelenie_list': podrazdelenie_list, 'current_lpu': current_lpu, 'new_list': new_list})
