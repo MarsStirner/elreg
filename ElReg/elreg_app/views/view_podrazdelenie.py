0046bc26393b0e4761ec7ef0ac8de4dfdab76ecf
Vintorez <Okulichev@rambler.ru>
2012-09-24 19:50:15 +0400

    Добавлено использование redis и вызов 404

--- ElReg/elreg_app/views/view_podrazdelenie.py
+++ ElReg/elreg_app/views/view_podrazdelenie.py
@@ -2,13 +2,16 @@
 #coding: utf-8
 
 from django.shortcuts import render_to_response
-from suds.client import Client
+from django.http import Http404
+from ElReg.settings import redis_db, client_info, client_list
 
-def index(request, template_name, podrazd='00'):
-    base_url = "http://10.1.2.107/int-server/index.php?wsdl=info"
-    client = Client(base_url)
+def index(request, template_name, podrazd='404'):
+    if podrazd == '404':
+        raise Http404()
+    if podrazd == '0':
+        podrazd = redis_db.get('podrazd')
     try:
-        x = client.service.getHospitalInfo()
+        x = client_info.service.getHospitalInfo()
     except:
         x = ''
     podrazdelenie_list = []
@@ -19,12 +22,17 @@ def index(request, template_name, podrazd='00'):
                 podrazdelenie_list.append(b.title)
 
     new_list = []
-    base_url = "http://10.1.2.107/int-server/index.php?wsdl=list"
-    client = Client(base_url)
-    y = client.service.listHospitals().hospitals
+    y = client_list.service.listHospitals().hospitals
     for v in y:
         for w in podrazdelenie_list:
             if v.uid.startswith('%s'%(podrazd)) and v.title == w:
-                new_list.append(v.uid)
+                new_list.append(v.uid.split('/')[1])
     new = zip(podrazdelenie_list, new_list)
-    return render_to_response(template_name, {'podrazdelenie_list': podrazdelenie_list, 'current_lpu': current_lpu, 'new_list': new_list, 'new': new})
+    redis_db.set('podrazd', podrazd)
+    redis_db.set('current_lpu', current_lpu)
+    redis_db.set('step', 3)
+    return render_to_response(template_name, {'podrazdelenie_list': podrazdelenie_list,
+                                              'current_lpu': current_lpu,
+                                              'new_list': new_list,
+                                              'new': new,
+                                              'step': redis_db.get('step')})
