26c7477d4e87dd914a3aa201f0e961090044accb
Vintorez <Okulichev@rambler.ru>
2012-09-24 19:52:33 +0400

    Добавлена возможность использования redis и изменена функциональность с учетом отката шагов

--- ElReg/elreg_app/views/view_lpu.py
+++ ElReg/elreg_app/views/view_lpu.py
@@ -2,25 +2,37 @@
 #coding: utf-8
 
 from django.shortcuts import render_to_response
-from suds.client import Client
 from elreg_app.models import Region
+from ElReg.settings import redis_db, client_info, client_list
 
-
-def index(request, template_name, okato='0'):
+def index(request, template_name, okato='not'):
     hospitals_list = []
     if okato == '0':
-        base_url = "http://10.1.2.107/int-server/index.php?wsdl=info"
-        client = Client(base_url)
-        info = client.service.getHospitalInfo()
-        for x in range(len(info)):
-            hospitals_list.append(info[x])
-    else:
-        base_url = "http://10.1.2.107/int-server/index.php?wsdl=list"
-        client = Client(base_url)
         try:
-            hospitals_list = client.service.listHospitals(ocatoCode=okato).hospitals
+            okato = redis_db.get('okato')
+            hospitals_list = client_list.service.listHospitals(ocatoCode=okato).hospitals
         except:
             hospitals_list = ''
-    current_region = '' if okato == '0' else Region.objects.get(code=okato)
+        redis_db.set('step', 2)
+    elif okato == 'not':
+        try:
+            info = client_info.service.getHospitalInfo()
+        except:
+            info = ''
+        for x in range(len(info)):
+            hospitals_list.append(info[x])
+        redis_db.set('step', 1)
+    else:
+        try:
+            hospitals_list = client_list.service.listHospitals(ocatoCode=okato).hospitals
+        except:
+            hospitals_list = ''
+        redis_db.set('step', 2)
+    current_region = '' if okato == 'not' else Region.objects.get(code=okato)
     region_list = Region.objects.filter(activation=True)
-    return render_to_response(template_name, {'hospitals_list': hospitals_list, 'region_list': region_list, 'current_region': current_region})
+    redis_db.set('okato', okato)
+    redis_db.set('current_region', current_region)
+    return render_to_response(template_name, {'hospitals_list': hospitals_list,
+                                              'region_list': region_list,
+                                              'current_region': current_region,
+                                              'step': redis_db.get('step')})
