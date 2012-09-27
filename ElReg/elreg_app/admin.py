4a8ae781bcd17ebfcc1fe1ca25627d9c3178d6ce
Vintorez <Okulichev@rambler.ru>
2012-09-27 19:27:41 +0400

    Добавлен комментарий к классу

--- ElReg/elreg_app/admin.py
+++ ElReg/elreg_app/admin.py
@@ -1,8 +1,13 @@
+#coding: utf-8
+
 from django.contrib import admin
 from models import Region
 
 
 class RegionAdmin(admin.ModelAdmin):
+    """
+    Класс, добавляющий в админку возможность добавлять (изменять, удалять) название МО и его код ОКАТО
+    """
     list_display = ('region', 'code', 'activation')
     search_fields = ('region', 'code')
 
