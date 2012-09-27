e5a1a347dd0c6ee6c9712caa5a2df4278d3264bc
Vintorez <Okulichev@rambler.ru>
2012-09-27 19:28:18 +0400

    Добавлен комментарий к модели

--- ElReg/elreg_app/models.py
+++ ElReg/elreg_app/models.py
@@ -1,6 +1,10 @@
+#coding: utf-8
 from django.db import models
 
 class Region (models.Model):
+    """
+    Модель для хранения данных в БД о названии МО и его коде ОКАТО
+    """
     region = models.CharField(max_length=100,)
     code = models.PositiveIntegerField(default=0)
     activation = models.BooleanField(default=True)
