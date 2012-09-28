487eb9e3b1c987d7ba4804347ca84bfb5ef35eea
Vintorez <Okulichev@rambler.ru>
2012-09-28 19:51:58 +0400

    Исправлена функциональность вывода таблицы расписания врача

--- ElReg/elreg_app/views/view_vremya.py
+++ ElReg/elreg_app/views/view_vremya.py
@@ -1,25 +1,30 @@
-#!/usr/bin/env python
 #coding: utf-8
 
 from django.shortcuts import render_to_response
 from ElReg.settings import redis_db, client_list, client_schedule
 import datetime
-import calendar
-import time
 
-def index(request, template_name, vremya='00'):
+def index(request, template_name, vremya='0'):
     """
     Логика страницы Время
     """
+    if vremya == '0' or vremya == '00' or vremya == '000':
+        if vremya == '00':
+            a = redis_db.get('firstweekday').split('-')
+            firstweekday = datetime.date(int(a[0]), int(a[1]), int(a[2])) + datetime.timedelta(days=7)
+        elif vremya == '000':
+            a = redis_db.get('firstweekday').split('-')
+            firstweekday = datetime.date(int(a[0]), int(a[1]), int(a[2])) - datetime.timedelta(days=7)
+        vremya = redis_db.get('vremya')
+    else:
+        now = datetime.date.today()
+        firstweekday = now - datetime.timedelta(days=datetime.date.isoweekday(now)-1)
     try:
         hospital_Uid = redis_db.get('hospital_Uid')
-        vr = client_schedule.service.getScheduleInfo(hospitalUid=hospital_Uid, doctorUid=vremya)
-        vr0 = []
-        for v in vr:
-            vr0.append((v.start.date(), v))
-        vr = vr0
+        ticketList = client_schedule.service.getScheduleInfo(hospitalUid=hospital_Uid, doctorUid=vremya)
     except:
-        vr = ''
+        ticketList = []
+
     current_podrazd = redis_db.get('current_podrazd')
     prof = redis_db.get('prof')
     docName = ''
@@ -27,69 +32,45 @@ def index(request, template_name, vremya='00'):
         if i.uid == vremya:
             docName = i.name
             redis_db.set('docName', docName)
-    week = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'ВС']
-    weeks = [0,1,2,3,4,5,6]
 
-
-    now = datetime.date.today()
-    x = datetime.date.isoweekday(now)
-    td1 = datetime.timedelta(days=x-1)
-    td2 = datetime.timedelta(days=7-x)
-    res1 = now - td1
-    res2 = now + td2
-#    res1 = res1 + datetime.timedelta(days=2)
-
-    times = []
-    dates = []
+    times = [] # Список времен начала записи текущей недели
+    dates = [] # Список дат текущей недели
 
     for i in xrange(7):
-        newDay = res1 + datetime.timedelta(days=i)
+        newDay = firstweekday + datetime.timedelta(days=i)
         dates.append(newDay)
-        for m in vr:
-            if newDay == m[1].start.date():
-                times.append((m[1].start.time(), m[1].finish.time()))
-    times = list(set(times))
-    times.sort()
+        for ticket in ticketList:
+            if newDay == ticket.start.date():
+                times.append(ticket.start.time())
+        times = list(set(times))
+        times.sort()
 
+    if not times:
+        ticketTable = []
+    else:
+        currentTicketList = []
+        for ticket in ticketList:
+            if ticket.start.date() in dates:
+                currentTicketList.append(ticket)
 
+        ticketTable = []
+        for time in times:
+            tmpList = [0,0,0,0,0,0,0]
+            for ticket in currentTicketList:
+                if ticket.start.time() == time:
+                    tmpList[dates.index(ticket.start.date())] = ticket
+            ticketTable.append(tmpList)
 
-    superList = []
-#    newSuperList = []
-    for time in times:
-        for date in dates:
-                for v in vr:
-                    if v[1].start.time() == time[0] and v[1].start.date() == date:
-                        superList.append(v[1].status)
-
-
-
-#    for v in xrange(0, len(superList), 7):
-#        newSuperList.append([superList[v+i] for i in xrange(7)])
-
-#            for v in xrange(0, len(vr), 7):
-#                superList = []
-#                for w in xrange(7):
-#                    if vr[v+w].start.time() == time and vr[v+w].start.date() == date:
-#                        superList.append(vr[v+w].status)
-#                    else:
-#                        superList.append('0')
-#                newSuperList.append(superList)
-
-#    for v in xrange(0, len(superList), 7):
-#        newSuperList.append([superList[v+i] for i in xrange(7)])
-
-
-
-
+    redis_db.set('vremya', vremya)
+    redis_db.set('docLastName', docName.lastName)
+    redis_db.set('docFirstName', docName.firstName)
+    redis_db.set('docPatronymic', docName.patronymic)
+    redis_db.set('firstweekday', firstweekday)
     redis_db.set('step', 6)
-    return render_to_response(template_name, {'vr': vr,
-                                              'current_podrazd': current_podrazd,
+    return render_to_response(template_name, {'current_podrazd': current_podrazd,
                                               'prof': prof,
                                               'docName': docName,
-                                              'week': week,
-                                              'weeks': weeks,
-                                              'times': times,
                                               'dates': dates,
-                                              'superList': superList,
-#                                              'newSuperList': newSuperList,
+                                              'times': times,
+                                              'ticketTable': ticketTable,
                                               'step': redis_db.get('step')})
