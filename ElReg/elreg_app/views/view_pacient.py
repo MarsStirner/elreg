2bb56c1beacba703b323514f4a19f772f4ef52c7
Vintorez <Okulichev@rambler.ru>
2012-09-28 19:49:04 +0400

    Добавлена базовая логика стриницы Пациент

--- ElReg/elreg_app/views/view_pacient.py
+++ ElReg/elreg_app/views/view_pacient.py
@@ -1,1 +1,25 @@
-#!/usr/bin/env python
+#coding: utf-8
+
+from django.shortcuts import render_to_response
+from ElReg.settings import redis_db, client_list, client_schedule
+import datetime
+
+def index(request, template_name, pacient='0'):
+    """
+    Логика страницы Пациент
+    """
+    try:
+        hospital_Uid = redis_db.get('hospital_Uid')
+        ticketList = client_schedule.service.getScheduleInfo(hospitalUid=hospital_Uid, doctorUid=vremya)
+    except:
+        ticketList = []
+
+    current_podrazd = redis_db.get('current_podrazd')
+    prof = redis_db.get('prof')
+    docName = [redis_db.get('docLastName'), redis_db.get('docFirstName'), redis_db.get('docPatronymic')]
+
+    redis_db.set('step', 7)
+    return render_to_response(template_name, {'current_podrazd': current_podrazd,
+                                              'prof': prof,
+                                              'docName': docName,
+                                              'step': redis_db.get('step')})
