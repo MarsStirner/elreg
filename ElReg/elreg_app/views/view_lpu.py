f0cb7727fecc7998450d3e874554ce327bd3a303
Vintorez <Okulichev@rambler.ru>
2012-10-02 22:23:26 +0400

    Удалена логика страницы МО и перенесена в файл view_mo.py

--- ElReg/elreg_app/views/view_lpu.py
+++ ElReg/elreg_app/views/view_lpu.py
@@ -2,39 +2,22 @@
 
 from django.shortcuts import render_to_response
 from elreg_app.models import Region
-from ElReg.settings import redis_db, client_info, client_list
+from ElReg.settings import redis_db, client
 
-def index(request, template_name, okato='not'):
+def index(request, template_name, okato=0):
     """
-    Логика страниц МО и ЛПУ
+    Логика страницы ЛПУ
     """
-    hospitals_list = []
-    if okato == '0':
-        try:
-            okato = redis_db.get('okato')
-            hospitals_list = client_list.service.listHospitals(ocatoCode=okato).hospitals
-        except:
-            hospitals_list = ''
-        redis_db.set('step', 2)
-    elif okato == 'not':
-        try:
-            info = client_info.service.getHospitalInfo()
-        except:
-            info = ''
-        for x in range(len(info)):
-            hospitals_list.append(info[x])
-        redis_db.set('step', 1)
-    else:
-        try:
-            hospitals_list = client_list.service.listHospitals(ocatoCode=okato).hospitals
-        except:
-            hospitals_list = ''
-        redis_db.set('step', 2)
-    current_region = '' if okato == 'not' else Region.objects.get(code=okato)
-    region_list = Region.objects.filter(activation=True)
-    redis_db.set('okato', okato)
+    if not okato:
+        okato = redis_db.get('okato')
+    try:
+        hospitals_list = client("list").service.listHospitals(ocatoCode=okato).hospitals
+    except:
+        hospitals_list = []
+    current_region = Region.objects.get(code=okato)
     redis_db.set('current_region', current_region)
+    redis_db.set('okato', okato)
+    redis_db.set('step', 2)
     return render_to_response(template_name, {'hospitals_list': hospitals_list,
-                                              'region_list': region_list,
                                               'current_region': current_region,
                                               'step': int(redis_db.get('step'))})
