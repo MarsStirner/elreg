7f6dae116688220f9c16841994ec4ff686dc14ce
Vintorez <Okulichev@rambler.ru>
2012-10-02 22:26:39 +0400

    Логика страницы Подразделение исправлена в сторону упрощения синтаксиса

--- ElReg/elreg_app/views/view_podrazdelenie.py
+++ ElReg/elreg_app/views/view_podrazdelenie.py
@@ -1,22 +1,20 @@
 #coding: utf-8
 
 from django.shortcuts import render_to_response
-from django.http import Http404
-from ElReg.settings import redis_db, client_info, client_list
+from ElReg.settings import redis_db, client
 
-def index(request, template_name, podrazd='404'):
+def index(request, template_name, podrazd=0):
     """
     Логика страницы Подразделение
     """
-    if podrazd == '404':
-        raise Http404()
-    if podrazd == '0':
+    if not podrazd:
         podrazd = redis_db.get('podrazd')
     try:
-        x = client_info.service.getHospitalInfo()
+        x = client("info").service.getHospitalInfo()
     except:
-        x = ''
+        x = []
     podrazdelenie_list = []
+    current_lpu = ''
     for c in x:
         if c.uid.startswith('%s'%(podrazd)):
             current_lpu = c
@@ -24,17 +22,15 @@ def index(request, template_name, podrazd='404'):
                 podrazdelenie_list.append(b.title)
 
     new_list = []
-    y = client_list.service.listHospitals().hospitals
+    y = client("list").service.listHospitals().hospitals
     for v in y:
         for w in podrazdelenie_list:
             if v.uid.startswith('%s'%(podrazd)) and v.title == w:
                 new_list.append(v.uid.split('/')[1])
-    new = zip(podrazdelenie_list, new_list)
+    podrazd_list = zip(podrazdelenie_list, new_list)
     redis_db.set('podrazd', podrazd)
     redis_db.set('current_lpu', current_lpu)
     redis_db.set('step', 3)
-    return render_to_response(template_name, {'podrazdelenie_list': podrazdelenie_list,
-                                              'current_lpu': current_lpu,
-                                              'new_list': new_list,
-                                              'new': new,
+    return render_to_response(template_name, {'current_lpu': current_lpu,
+                                              'podrazd_list': podrazd_list,
                                               'step': int(redis_db.get('step'))})
