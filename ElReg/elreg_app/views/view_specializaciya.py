7dee65472735526eb17f56e57243fc93509a546c
Vintorez <Okulichev@rambler.ru>
2012-10-03 22:14:16 +0400

    Подкорректирована логика в связи с отсутствием необходимости передавать лишнюю переменную в адрес и в связи с изменением типа хранимых данных в redis

--- ElReg/elreg_app/views/view_specializaciya.py
+++ ElReg/elreg_app/views/view_specializaciya.py
@@ -4,33 +4,32 @@ from django.shortcuts import render_to_response
 from django.http import Http404
 from ElReg.settings import redis_db, client
 
-def index(request, template_name, spec='404'):
+def index(request, template_name, spec=0):
     """
     Логика страницы Специализация
     """
-    if spec == '404':
-        raise Http404()
-    if spec == '0':
-        spec = redis_db.get('spec')
+    id = '%s' % request.session.session_key
+    if not spec:
+        spec = redis_db.hget(id, 'spec')
     try:
         y = client("list").service.listDoctors()
     except:
-        y = ''
+        y = []
     spc = []
     for i in y.doctors:
-        if i.hospitalUid == "%s/%s"%(redis_db.get('podrazd'), spec):
+        if i.hospitalUid == "%s/%s"%(redis_db.hget(id, 'podrazd'), spec):
             spc.append(i.speciality)
             spc = list(set(spc))
 
     current_podrazd = ''
 
     for i in y.hospitals:
-        if i.uid == "%s/%s"%(redis_db.get('podrazd'), spec):
+        if i.uid == "%s/%s"%(redis_db.hget(id, 'podrazd'), spec):
             current_podrazd = i.title
             break
-    redis_db.set('spec', spec)
-    redis_db.set('current_podrazd', current_podrazd)
-    redis_db.set('step', 4)
+    redis_db.hset(id, 'spec', spec)
+    redis_db.hset(id, 'current_podrazd', current_podrazd)
+    redis_db.hset(id, 'step', 4)
     return render_to_response(template_name, {'spc': spc,
                                               'current_podrazd': current_podrazd,
-                                              'step': int(redis_db.get('step'))})
+                                              'step': int(redis_db.hget(id, 'step'))})
