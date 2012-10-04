b98fd610c99e08f64a668ca7593960be3c805ceb
Vintorez <Okulichev@rambler.ru>
2012-10-04 22:54:52 +0400

    Добавлен базовый функционал для страницы поиска ЛПУ

--- ElReg/elreg_app/views/view_lpu.py
+++ ElReg/elreg_app/views/view_lpu.py
@@ -9,16 +9,21 @@ def index(request, template_name, okato=0):
     Логика страницы ЛПУ
     """
     id = '%s' % request.session.session_key
-    if not okato:
-        okato = redis_db.hget(id, 'okato')
-    try:
-        hospitals_list = client("list").service.listHospitals(ocatoCode=okato).hospitals
-    except:
-        hospitals_list = []
-    current_region = Region.objects.get(code=okato)
-    redis_db.hset(id, 'current_region', current_region)
-    redis_db.hset(id, 'okato', okato)
-    redis_db.hset(id, 'step', 2)
-    return render_to_response(template_name, {'hospitals_list': hospitals_list,
-                                              'current_region': current_region,
-                                              'step': int(redis_db.hget(id, 'step'))})
+    if okato == "search":
+        redis_db.hset(id, 'step', 2)
+        return render_to_response(template_name, {
+                                                  'step': int(redis_db.hget(id, 'step'))})
+    else:
+        if not okato:
+            okato = redis_db.hget(id, 'okato')
+        try:
+            hospitals_list = client("list").service.listHospitals(ocatoCode=okato).hospitals
+        except:
+            hospitals_list = []
+        current_region = Region.objects.get(code=okato)
+        redis_db.hset(id, 'current_region', current_region)
+        redis_db.hset(id, 'okato', okato)
+        redis_db.hset(id, 'step', 2)
+        return render_to_response(template_name, {'hospitals_list': hospitals_list,
+                                                  'current_region': current_region,
+                                                  'step': int(redis_db.hget(id, 'step'))})
