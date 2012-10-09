34825774ca30ecc00fa7fe5009a651cd272d3613
Vintorez <Okulichev@rambler.ru>
2012-10-09 22:02:19 +0400

    Добавлена проверка на наличие у пользователя идентификатора сессии

--- ElReg/elreg_app/views/view_mo.py
+++ ElReg/elreg_app/views/view_mo.py
@@ -1,5 +1,6 @@
 #coding: utf-8
 
+from django.contrib.sessions.backends.db import SessionStore
 from django.shortcuts import render_to_response
 from elreg_app.models import Region
 from ElReg.settings import redis_db, client
@@ -8,11 +9,14 @@ def index(request, template_name):
     """
     Логика страницы МО
     """
-    id = '%s' % request.session.session_key
-    print id, "<<<<<<<<<<--sessionid"
+    # проверка на наличие у пользователя идентификатора сессии
+    # и создание сессии пользователя в случае отсутствия идентификатора
+    id = request.session.session_key
+    if not id:
+        s = SessionStore()
+        s.save()
+        id = s.session_key
     redis_db.hset(id, 'step', 1)
-#    s = request.COOKIES['sessionid']
-#    print s, "<-------"
 
     region_list = Region.objects.filter(activation=True)
     return render_to_response(template_name, {'region_list': region_list,
