d9f34190a0b9077c0b1723127577d1e60d19498c
Vintorez <Okulichev@rambler.ru>
2012-10-11 23:34:01 +0400

    Добавлен url-адрес для представления search

--- ElReg/ElReg/urls.py
+++ ElReg/ElReg/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from elreg_app.views import view_mo, view_lpu, view_podrazdelenie, view_specializaciya, view_vrach, view_vremya, view_pacient, view_zapis, view_updates
+from elreg_app.views import view_mo, view_lpu, view_podrazdelenie, view_specializaciya, view_vrach, view_vremya, view_pacient, view_zapis, view_updates, view_search
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', view_mo.index, {'template_name': 'mo.html'}, name="mo"),
     url(r'^updates/$', view_updates.index),
+    url(r'^search/$', view_search.index),
     url(r'^lpu/$', view_lpu.index, {'template_name': 'lpu.html'}, name="lpu"),
     url(r'^lpu/(?P<okato>\d{11}|search)/$', view_lpu.index, {'template_name': 'lpu.html'}),
     url(r'^podrazdelenie/$', view_podrazdelenie.index, {'template_name': 'podrazdelenie.html'}, name="podrazdelenie"),
@@ -15,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^specializaciya/(?P<spec>\d{2,13})/$', view_specializaciya.index, {'template_name': 'specializaciya.html'}),
     url(r'^vrach/$', view_vrach.index, {'template_name': 'vrach.html'}, name="vrach"),
     url(r'^vremya/$', view_vremya.index, {'template_name': 'vremya.html'}, name="vremya"),
-    url(r'^vremya/(?P<vremya>\d{3}|next|prev)/$', view_vremya.index, {'template_name': 'vremya.html'}),
+    url(r'^vremya/(?P<vremya>\d{2,3}|next|prev)/$', view_vremya.index, {'template_name': 'vremya.html'}),
     url(r'^pacient/$', view_pacient.index, {'template_name': 'pacient.html'}, name="pacient"),
     url(r'^zapis/$', view_zapis.index, {'template_name': 'zapis.html'}, name="zapis"),
 
