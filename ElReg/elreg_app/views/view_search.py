652b1e909a6a7894807fba78a94fc2403749e991
Vintorez <Okulichev@rambler.ru>
2012-10-12 23:54:20 +0400

    Добавлена возможность динамического поиска по нескольким словам и изменен способ обращения к wsdl-файлам

--- ElReg/elreg_app/views/view_search.py
+++ ElReg/elreg_app/views/view_search.py
@@ -1,65 +1,42 @@
 #coding: utf-8
 
-from django.http import HttpResponse, Http404
-from ElReg.settings import redis_db, client
+from django.http import HttpResponse, HttpResponseRedirect
+from elreg_app.functions import InfoWSDL
 import json
 
 def index(request):
     """ Логика страницы search
     Страница создана только для динамической подгрузки данных при помощи AJAX'а.
     Вызывается на вкладке "ЛПУ".
-    Запуск через адресную строку приведет к ошибке 404.
+    Запуск через адресную строку приведет к редиректу на главную страницу.
     """
     if request.method == 'GET':
-        GET = request.GET
-        new = {}
-        print GET.items(), "<<<<<<"
+        if request.GET.has_key('search'):
+            hospitals_list = InfoWSDL().getHospitalInfo()
 
-#        try:
-#            hospitals_list = client("list").service.listHospitals(ocatoCode=okato).hospitals
-#        except:
-#            hospitals_list = []
+            # получение спика введенных пользователем слов
+            search_list = request.GET.get( 'search' ).lower().split(' ')
 
-        # Добавить реализацию 404 ошибки !!!
+            # формирование временного списка кортежей [(uid ЛПУ, наименование ЛПУ), ...]
+            tmp_list = []
+            for i in hospitals_list:
+                tmp_list.append((i.uid.split('/')[0], i.title.lower()))
 
-        if GET.has_key('search'):
-            search = request.GET.get( 'search' )
-            new[1] = search
-        response = HttpResponse()
-        response['Content_Type'] = "text/javascript"
-        response.write(json.dumps(new))
-        return response
+            # формирование словаря со значениями, удовлетворяющими поиску,
+            # где ключ - uid ЛПУ, а значение - наименование ЛПУ
+            lpu_dict ={}
+            for (uid,title) in tmp_list:
+                flag = True
+                for i in search_list:
+                    if title.find(i) == -1:
+                        flag = False
+                if flag:
+                    lpu_dict[uid] = title
 
-#    id = '%s' % request.session.session_key
-#    try:
-#        y = client("list").service.listDoctors()
-#    except:
-#        y = ''
-#    tmp, new = [], {}
-#
-#    if 'clickSpec' in request.GET:
-#        spec = request.GET['clickSpec']
-#        redis_db.hset(id, 'spec', spec)
-#        for i in y.doctors:
-#            if i.hospitalUid == "%s/%s"%(redis_db.hget(id, 'podrazd'), spec):
-#                tmp.append(i.speciality)
-#                tmp = list(set(tmp))
-#        new = dict(zip(xrange(len(tmp)), tmp))
-#
-#    elif 'clickProf' in request.GET:
-#        prof = request.GET['clickProf']
-#        hospital_Uid = "%s/%s"%(redis_db.hget(id, 'podrazd'), redis_db.hget(id, 'spec'))
-#        for i in y.doctors:
-#            if i.hospitalUid == hospital_Uid and i.speciality == prof:
-#                tmp.append(i)
-#        for i in tmp:
-#            new[i.uid] = '%s %s %s' % (i.name.lastName, i.name.firstName, i.name.patronymic)
-#    else:
-#        raise Http404
-        # создание ответа в формате json:
-#    new = {1:"qw", 2:"er", 3:"ty"}
-#    response = HttpResponse()
-#    response['Content_Type'] = "text/javascript"
-##    response.write(json.dumps(new))
-#    response.write({query:'Li',suggestions:['Liberia','Libyan Arab Jamahiriya','Liechtenstein','Lithuania'],data:['LR','LY','LI','LT']})
-#    return response
+            # создание ответа в формате json:
+            response = HttpResponse()
+            response['Content_Type'] = "text/javascript"
+            response.write(json.dumps(lpu_dict))
+            return response
+    else:
+        return HttpResponseRedirect("/")
