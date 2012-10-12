8d58ead2326982082764046dbd3d7cdb4f87d0ef
Vintorez <Okulichev@rambler.ru>
2012-10-12 23:55:32 +0400

    Изменен способ обращения к wsdl-файлам через классы, расположенные в functions.py

--- ElReg/elreg_app/views/view_updates.py
+++ ElReg/elreg_app/views/view_updates.py
@@ -1,26 +1,24 @@
 #coding: utf-8
 
-from django.http import HttpResponse, Http404
-from ElReg.settings import redis_db, client
+from django.http import HttpResponse, HttpResponseRedirect
+from ElReg.settings import redis_db
+from elreg_app.functions import ListWSDL
 import json
 
 def index(request):
     """ Логика страницы updates
     Страница создана только для динамической подгрузки данных при помощи AJAX'а.
     Вызывается на вкладке "Подразделение/Специализация/Врач".
-    Запуск через адресную строку приведет к ошибке 404.
+    Запуск через адресную строку приведет к редиректу на главную страницу.
     """
-    id = '%s' % request.session.session_key
-    try:
-        y = client("list").service.listDoctors()
-    except:
-        y = ''
+    id = request.session.session_key
+    doctors_list = ListWSDL().listDoctors()
     tmp, new = [], {}
 
     if 'clickSpec' in request.GET:
         spec = request.GET['clickSpec']
         redis_db.hset(id, 'spec', spec)
-        for i in y.doctors:
+        for i in doctors_list:
             if i.hospitalUid == "%s/%s"%(redis_db.hget(id, 'podrazd'), spec):
                 tmp.append(i.speciality)
                 tmp = list(set(tmp))
@@ -29,13 +27,13 @@ def index(request):
     elif 'clickProf' in request.GET:
         prof = request.GET['clickProf']
         hospital_Uid = "%s/%s"%(redis_db.hget(id, 'podrazd'), redis_db.hget(id, 'spec'))
-        for i in y.doctors:
+        for i in doctors_list:
             if i.hospitalUid == hospital_Uid and i.speciality == prof:
                 tmp.append(i)
         for i in tmp:
             new[i.uid] = '%s %s %s' % (i.name.lastName, i.name.firstName, i.name.patronymic)
     else:
-        raise Http404
+        return HttpResponseRedirect("/")
     # создание ответа в формате json:
     response = HttpResponse()
     response['Content_Type'] = "text/javascript"
