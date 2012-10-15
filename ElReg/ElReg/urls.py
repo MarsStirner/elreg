b9ef549729349f07d1e62ffa0981740fac72c972
Vintorez <Okulichev@rambler.ru>
2012-10-15 23:38:31 +0400

    Изменены пути к вьюхам

--- ElReg/ElReg/urls.py
+++ ElReg/ElReg/urls.py
@@ -1,26 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from elreg_app.views import view_mo, view_lpu, view_podrazdelenie, view_vremya, view_pacient, view_zapis, view_updates, view_search
 admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('elreg_app.views',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', view_mo.index, {'template_name': 'mo.html'}, name="mo"),
-    url(r'^updates/$', view_updates.index),
-    url(r'^search/$', view_search.index),
-    url(r'^lpu/$', view_lpu.index, {'template_name': 'lpu.html'}, name="lpu"),
-    url(r'^lpu/(?P<okato>\d{11}|search)/$', view_lpu.index, {'template_name': 'lpu.html'}),
-    url(r'^podrazdelenie/$', view_podrazdelenie.index, {'template_name': 'podrazdelenie.html'}, name="podrazdelenie"),
-    url(r'^podrazdelenie/(?P<podrazd>\d{1,2})/$', view_podrazdelenie.index, {'template_name': 'podrazdelenie.html'}),
-    url(r'^vremya/$', view_vremya.index, {'template_name': 'vremya.html'}, name="vremya"),
-    url(r'^vremya/(?P<vremya>\d{2,3}|next|prev)/$', view_vremya.index, {'template_name': 'vremya.html'}),
-    url(r'^pacient/$', view_pacient.index, {'template_name': 'pacient.html'}, name="pacient"),
-    url(r'^zapis/$', view_zapis.index, {'template_name': 'zapis.html'}, name="zapis"),
-
-    # Examples:
-    # url(r'^$', 'ElReg.views.home', name='home'),
-    # url(r'^ElReg/', include('ElReg.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'moPage', {'template_name': 'mo.html'}, name="mo"),
+    url(r'^lpu/$', 'lpuPage', {'template_name': 'lpu.html'}, name="lpu"),
+    url(r'^lpu/(?P<okato>\d{11}|search)/$', 'lpuPage', {'template_name': 'lpu.html'}),
+    url(r'^search_lpu/$', 'searchLpuPage'),#ajax only
+    url(r'^search_gorod/$', 'searchGorodPage'),#ajax only
+    url(r'^podrazdelenie/$', 'podrazdeleniePage', {'template_name': 'podrazdelenie.html'}, name="podrazdelenie"),
+    url(r'^podrazdelenie/(?P<podrazd>\d{1,2})/$', 'podrazdeleniePage', {'template_name': 'podrazdelenie.html'}),
+    url(r'^updates/$', 'updatesPage'),#ajax only
+    url(r'^vremya/$', 'vremyaPage', {'template_name': 'vremya.html'}, name="vremya"),
+    url(r'^vremya/(?P<vremya>\d{2,3}|next|prev)/$', 'vremyaPage', {'template_name': 'vremya.html'}),
+    url(r'^pacient/$', 'pacientPage', {'template_name': 'pacient.html'}, name="pacient"),
+    url(r'^zapis/$', 'zapisPage', {'template_name': 'zapis.html'}, name="zapis"),
     )
