b7e9bf54305f6b89e900f966b758f145ab7fb25d
vintorez <okulichev@rambler.ru>
2012-10-18 23:27:55 +0400

    Добавлен url-адрес для sentry

--- ElReg/urls.py
+++ ElReg/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from settings import APP_NAME
+from settings import APP_NAME, DEBUG
 admin.autodiscover()
 urlpatterns = patterns('',
     # admin:
     url(r'^admin/', include(admin.site.urls)),
     # electronic regisrtature application:
     url(r'^', include(APP_NAME + '.urls')),
+    )
+
+# sentry:
+if not DEBUG:
+    urlpatterns += patterns('',
+        url(r'^sentry/', include('sentry.web.urls')),
     )
