1469c5a7fb4d72e3a1e39ea4291051120013fe00
vintorez <okulichev@rambler.ru>
2012-10-24 23:30:16 +0400

    Переписан вызов AJAX'а. Высокоуровневый запрос .getJSON заменен на низкоуровневый запрос .ajax в связи с тем, что IE может корректно обрабатывать запросы только со значением cache = false

--- ElReg/elreg_app/static/js/updates.js
+++ ElReg/elreg_app/static/js/updates.js
@@ -1,3 +1,5 @@
+jQuery.support.cors = true;
+
 $(document).ready(function () {
     $('a.spec').live("click", function() {
         $('.specActive').removeClass('specActive').addClass('spec');
@@ -6,15 +8,22 @@ $(document).ready(function () {
         var $value = $(this).text();
         $('table.secondTable tr').hide();
         $('table.thirdTable tr').hide();
-        $.getJSON("/updates/", {clickSpec: $clickSpec, value: $value}, function(data) {
-            var $items = [];
-            $.each(data, function(key, val) {
-                $items.push('<tr><td><a class="prof">' + val + '</a></td></tr>');
-            });
-            $($items.join('')).fadeIn('fast').appendTo('table.secondTable');
-        $('body,html').animate({
-            scrollTop: 0
-        }, 300);
+        $.ajax({
+            url: '/updates/',
+            data: {clickSpec: $clickSpec, value: $value},
+            crossDomain: true,
+            cache: false, // обязательно для IE
+            dataType: 'json',
+            success: function (data) {
+                var $items = [];
+                $.each(data, function(key, val) {
+                    $items.push('<tr><td><a class="prof">' + val + '</a></td></tr>');
+                });
+                $($items.join('')).fadeIn('fast').appendTo('table.secondTable');
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 300);
+            }
         });
     });
 
@@ -23,15 +32,22 @@ $(document).ready(function () {
         $(this).addClass('profActive');
         var $clickProf = $(this).text();
         $('table.thirdTable tr').hide();
-        $.getJSON("/updates/", {clickProf: $clickProf}, function(data) {
-            var $items = [];
-            $.each(data, function(key, val) {
-                $items.push('<tr><td><a href="/time/' + key + '"/">' + val + '</a></td></tr>');
-            });
-            $($items.join('')).fadeIn('fast').appendTo('table.thirdTable');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 300);
+        $.ajax({
+            url: '/updates/',
+            data: {clickProf: $clickProf},
+            crossDomain: true,
+            cache: false, // обязательно для IE
+            dataType: 'json',
+            success: function (data) {
+                var $items = [];
+                $.each(data, function(key, val) {
+                    $items.push('<tr><td><a href="/time/' + key + '"/">' + val + '</a></td></tr>');
+                });
+                $($items.join('')).fadeIn('fast').appendTo('table.thirdTable');
+                $('body,html').animate({
+                    scrollTop: 0
+                }, 300);
+            }
         });
     });
 });
