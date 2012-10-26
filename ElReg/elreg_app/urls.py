b8872d2c70b033576e3014acaf28d1daf4fd03a4
vintorez <okulichev@rambler.ru>
2012-10-26 23:18:09 +0400

    Заменены с транслита на английский названия представлений

--- ElReg/elreg_app/urls.py
+++ ElReg/elreg_app/urls.py
@@ -1,18 +1,20 @@
+# -*- coding: utf-8 -*-
+
 from django.conf.urls import patterns, url
 from settings import APP_NAME
 
 urlpatterns = patterns(APP_NAME + '.views',
-    # site:
-    url(r'^$', 'moPage', {'template_name': 'mo.html'}, name="mo"),
-    url(r'^medical_institution/$', 'lpuPage', {'template_name': 'lpu.html'}, name="lpu"),
-    url(r'^medical_institution/(?P<okato>\d{11}|search)/$', 'lpuPage', {'template_name': 'lpu.html'}),
-    url(r'^subdivision/$', 'podrazdeleniePage', {'template_name': 'podrazdelenie.html'}, name="podrazdelenie"),
-    url(r'^subdivision/(?P<podrazd>\d{1,2})/$', 'podrazdeleniePage', {'template_name': 'podrazdelenie.html'}),
-    url(r'^time/$', 'vremyaPage', {'template_name': 'vremya.html'}, name="vremya"),
-    url(r'^time/(?P<vremya>\d{2,3}|next|prev)/$', 'vremyaPage', {'template_name': 'vremya.html'}),
-    url(r'^patient/$', 'pacientPage', {'template_name': 'pacient.html'}, name="pacient"),
-    url(r'^register/$', 'zapisPage', {'template_name': 'zapis.html'}, name="zapis"),
-    # ajax only:
+    # страницы сайта:
+    url(r'^$', 'indexPage', {'templateName': 'mo.html'}, name="mo"),
+    url(r'^medical_institution/$', 'medicalInstitutionPage', {'templateName': 'lpu.html'}, name="lpu"),
+    url(r'^medical_institution/(?P<okato>\d{11}|search)/$', 'medicalInstitutionPage', {'templateName': 'lpu.html'}),
+    url(r'^subdivision/$', 'subdivisionPage', {'templateName': 'podrazdelenie.html'}, name="podrazdelenie"),
+    url(r'^subdivision/(?P<sub>\d{1,2})/$', 'subdivisionPage', {'templateName': 'podrazdelenie.html'}),
+    url(r'^time/$', 'timePage', {'templateName': 'vremya.html'}, name="vremya"),
+    url(r'^time/(?P<vremya>\d{2,3}|next|prev)/$', 'timePage', {'templateName': 'vremya.html'}),
+    url(r'^patient/$', 'patientPage', {'templateName': 'pacient.html'}, name="pacient"),
+    url(r'^register/$', 'registerPage', {'templateName': 'zapis.html'}, name="zapis"),
+    # адреса, используемые только AJAX'ом:
     url(r'^search/$', 'searchPage'),
     url(r'^updates/$', 'updatesPage'),
 )
