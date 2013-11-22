d4a56ab49f380e402834373ea4d6efbe0e767c56
SAntipov <santipov@korusconsulting.ru>
2013-11-22 16:34:22 +0400

    ELREG-148: секунды при передачи талончика в ИС

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-version = u'2.2.5'
-last_change_date = datetime(year=2013, month=11, day=22, hour=13,)
+version = u'2.2.6'
+last_change_date = datetime(year=2013, month=11, day=22, hour=16,)
