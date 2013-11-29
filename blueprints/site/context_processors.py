1a00e241b9663f6449f1e1ef7c87fcfecd6a7f83
SAntipov <santipov@korusconsulting.ru>
2013-11-29 12:53:35 +0400

    PGU-103: поле в настройках для ввода кода Яндекс.Метрики

--- blueprints/site/context_processors.py
+++ blueprints/site/context_processors.py
@@ -8,4 +8,5 @@ def header():
     return dict(site_name=_config('SITE_NAME'),
                 region_name=_config('REGION_NAME'),
                 logo=_config('LOGO_FILE'),
+                counter=_config('COUNTER_CODE'),
                 home_link=_config('HOME_LINK'))
